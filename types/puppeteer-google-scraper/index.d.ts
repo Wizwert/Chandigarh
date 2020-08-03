0f0f5320b17d9beaab4e8d12db3ad5c3adaf2c61
Will <william.rusinko@gmail.com>
2020-08-03 11:22:36 +0100

    Add type defs for puppeteer-google-scraper

---
+++ types/puppeteer-google-scraper/index.d.ts
@@ -0,0 +1,13 @@
+export interface IPuppeteerGoogleScraperOptions {
+  limit?: number;
+  headless?: boolean;
+  debugDir?: string;
+  searchUrl?: string;
+}
+
+export interface ISearchResult {
+  title: string;
+  url: string;
+}
+
+export default function search(term: string, options?: IPuppeteerGoogleScraperOptions): ISearchResult[];
