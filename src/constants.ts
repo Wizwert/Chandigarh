4a0064d37546a5473ebbf69f75af958096b12b3f
Will <william.rusinko@gmail.com>
2020-10-06 09:14:13 +0100

    Including additional domain

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
 export const chandigarhSheetID : string = '1aCNSfTKJqLboNnajcaSosN05YVy2QhX3jr9UzqrFrLQ';
 export const automationSheetID : string = '1sRA3f7knFNXpNTcadw_SRv5zzf5oaQ8N8I1xuc8f4sg';
 
-export const domainsToInclude : string[] = ['https://www.dorotheum.com/'];
+export const domainsToInclude : string[] = ['https://www.dorotheum.com/', 'https://www.bonhams.com/'];
